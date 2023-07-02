85773308a44a8f175fcbb50c9119613ba97e52dd
Vitoria Damasceno <83039172+vitoriadaamasceno@users.noreply.github.com>
2023-07-02 10:32:02 -0300

    Update metodos_estaticos.js

--- metodos_estaticos.js
+++ metodos_estaticos.js
@@ -1,4 +1,5 @@
 //Metodos Estaticos : permite acessar metodos de uma classe sem criar um objeto antes.
+//A palavra chave static define um método estático para a classe. Métodos estáticos não são chamados na instâncias da classe. Em vez disso, eles são chamados na própria classe. Geralmente, são funções utilitárias, como funções para criar ou clonar objetos.
 
 class Calculadora {
 
